07c2cc455923cefc16a57f7ac06095f8916cfd13
Mike Slinn <mslinn@mslinn.com>
2023-11-28 07:43:54 -0500

    -

--- spec/trim_spec.rb
+++ spec/trim_spec.rb
@@ -1,13 +1,16 @@
 require_relative '../lib/trim'
 
 RSpec.describe(MediaTrim) do
+  before(:example) do
+    @media_trim = described_class.new
+    @media_trim.set_defaults
+    @media_trim.fname = 'demo/demo.mp4'
+    @media_trim.copy_filename = 'demo/trim.demo.mp4'
+    @media_trim.start = '1'
+    @media_trim.interval = ['-ss', @start, '-to', '2']
+  end
+
   it 'does nothing' do
-    media_trim = described_class.new
-    media_trim.set_defaults
-    media_trim.fname = 'demo/demo.mp4'
-    media_trim.copy_filename = 'demo/trim.demo.mp4'
-    media_trim.start = '1'
-    media_trim.interval = ['-ss', @start, '-to', '2']
     expect(true).to be_truthy
   end
 end
